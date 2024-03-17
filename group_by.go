e4818fb340f025e89064a751c70e0dfcd9c5e32a
Saeki <saeki-hisafumi@moiterika.jp>
2024-03-17 14:30:28 +0900

    FIX GroupBy

--- group_by.go
+++ group_by.go
@@ -3,11 +3,7 @@ package a
 func GroupBy[K comparable, T any](src []T, k func(T) K) map[K][]T {
 	ret := make(map[K][]T, len(src))
 	for _, e := range src {
-		if _, exists := ret[k(e)]; exists {
-			ret[k(e)] = append(ret[k(e)], e)
-		} else {
-			ret[k(e)] = []T{e}
-		}
+		ret[k(e)] = []T{e}
 	}
 	return ret
 }
