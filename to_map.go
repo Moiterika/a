0a2f0327f864f682d36abc68aca5dbab68938d40
Saeki <saeki-hisafumi@moiterika.jp>
2024-03-18 17:21:23 +0900

    WIP

--- to_map.go
+++ to_map.go
@@ -2,6 +2,9 @@ package a
 
 func ToMap[K comparable, T any](src []T, k func(T) K) map[K]T {
 	ret := make(map[K]T, len(src))
+	if k == nil {
+		return ret
+	}
 	for _, e := range src {
 		ret[k(e)] = e
 	}
